a47a84dfc7d29b8223c01b31ec1b389fe10689a7
Geovani Mendonça <geovani.torezin11@gmail.com>
2022-01-18 16:39:40 -0300

    Subindo primeira Query SQL

---
+++ SQLQuery4.sql
@@ -0,0 +1,114 @@
+/****** Script do comando SelectTopNRows de SSMS  ******/
+SELECT  /* selecionando colunas de uma tabela*/
+	FirstName,
+	LastName
+FROM Person.Person
+
+SELECT DISTINCT		/* Distinct (Omite dados duplicados, retorna apenas dados unicos) */
+	FirstName
+FROM Person.Person
+
+SELECT DISTINCT /* exercicio basico de distinct */
+	LastName
+FROM Person.Person
+
+-- WHERE
+SELECT * 
+FROM Person.Person
+WHERE FirstName = 'Rob' and LastName = 'Caron'
+
+SELECT *
+FROM production.Product
+WHERE Color = 'blue' or color = 'black'
+
+SELECT *
+FROM production.Product
+WHERE ListPrice > 1500 and ListPrice < 5000
+
+SELECT *
+FROM production.Product
+WHERE Color <> 'red' -- diferente
+
+SELECT -- Desafio 1 - peças que pesam mais de 500Kg e menos que 700Kg
+	Name
+FROM production.Product
+WHERE Weight > 500 and Weight <= 700 
+
+SELECT * -- Desafio 2 - Funcionarios casados e asalariados
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'M' and SalariedFlag = 1
+
+SELECT * -- Desafio 3 - Conseguir o e-mail do Peter Krebs
+FROM Person.Person
+WHERE FirstName = 'Peter' and LastName = 'Krebs'
+
+SELECT EmailAddress
+FROM Person.EmailAddress
+WHERE BusinessEntityID = 26
+
+-- COUNT
+SELECT COUNT(DISTINCT Title)
+FROM Person.Person
+
+SELECT COUNT(*) -- Desafio 1 - quantos produtos temos cadastrados em nossa tabela de produtos
+FROM Production.Product
+
+SELECT COUNT(Size) -- Desafio 2 - quantos tamanhos de produtos
+FROM Production.Product
+
+SELECT COUNT(DISTINCT Size) -- Desafio 3 - quantos tamanhos diferentes de produtos
+FROM Production.Product
+
+-- TOP
+
+SELECT TOP  100 *
+FROM Person.Person
+
+-- ORDER BY
+
+SELECT *
+FROM Person.Person
+ORDER BY FirstName asc --desc
+
+SELECT FirstName, LastName
+FROM Person.Person
+ORDER BY FirstName asc,LastName desc 
+
+-- Desafio 1 - Obter o ProductID dos 10 produtos mais caros cadastrados no sistema, listando do mais caro pro mais barato
+SELECT *
+FROM Production.Product
+
+SELECT TOP 10  ProductId, ListPrice
+FROM Production.Product
+ORDER BY ListPrice desc
+
+SELECT TOP 4 name, ProductNumber
+FROM Production.Product
+ORDER BY ProductID asc
+
+-- BETWEEN (valor entre)
+
+SELECT * 
+FROM Production.Product
+WHERE ListPrice between 1000 and 1500;
+
+SELECT * 
+FROM Production.Product
+WHERE ListPrice NOT between 1000 and 1500;
+
+SELECT * 
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' and '2010/01/01'
+ORDER BY HireDate desc
+ 
+-- IN (valor contém)
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID IN (2,7,13)
+
+-- LIKE (buscar algo parecido no banco de dados)
+SELECT *
+FROM Person.Person
+WHERE FirstName like '%essa%'
+
+-- Desafios
