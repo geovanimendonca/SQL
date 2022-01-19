9235a3a7b00ebaf50728814470076ba33bab6562
Geovani Mendonça <geovani.torezin11@gmail.com>
2022-01-19 14:28:23 -0300

    query mais atual

--- SQLQuery_avançado.sql
+++ SQLQuery_avançado.sql
@@ -14,3 +14,32 @@ SELECT TOP 10 *
 FROM Person.BusinessEntityAddress BA
 INNER JOIN Person.Address PA ON PA.AddressID = BA.AddressID
 
+-- Desafio
+SELECT TOP 10 *
+FROM Person.PhoneNumberType
+
+SELECT TOP 10 *
+FROM Person.PersonPhone
+
+SELECT PP.BusinessEntityID, PN.Name, PP.PhoneNumberTypeID, PP.PhoneNumber
+FROM Person.PhoneNumberType PN
+INNER JOIN Person.PersonPhone PP ON PN.PhoneNumberTypeID = PN.PhoneNumberTypeID
+WHERE PN.Name ='HOME'
+ORDER BY PP.BusinessEntityID ASC
+
+SELECT PA.AddressID, PA.City, PA.StateProvinceID, PS.Name
+FROM Person.StateProvince PS
+INNER JOIN Person.Address PA ON PS.StateProvinceID = PA.StateProvinceID
+
+-- LEFT OUTER JOIN
+-- LEFT JOIN
+-- Descobrir quais pessoas tem um cartão de crédito registrado
+SELECT * 
+FROM Person.Person PP
+LEFT JOIN Sales.PersonCreditCard PC ON PP.BusinessEntityID = PC.BusinessEntityID
+WHERE PC.BusinessEntityID IS NULL
+-- INNER JOIN : 19118 linhas
+-- LEFT JOIN : 19972
+
+
+-- UNION
